0badd882a1258082368c65a78776100dfbb3b5f4
chaeyoo <chaeh0124@gmail.com>
2024-06-09 00:08:19 +0900

    [FEAT] 퀴즈 생성 테스트 추가

--- src/components/CreateQuiz.spec.tsx
+++ src/components/CreateQuiz.spec.tsx
@@ -7,25 +7,74 @@ const renderComponent = () => {
 
 	const wordinput = screen.getByPlaceholderText(/단어/i);
 	const meaningInput = screen.getByPlaceholderText(/뜻/i);
-
-	const descErrorMessage = screen.getByText("단어를 입력해주세요.");
-	const payerErrorMessage = screen.getByText("뜻을 입력해주세요.");
+	const addButton = screen.getByText("추가하기");
+	const wordErrorMessage = screen.getByText("단어를 입력해주세요.");
+	const meaningErrorMessage = screen.getByText("뜻을 입력해주세요.");
 
 	return {
 		wordinput,
 		meaningInput,
-		descErrorMessage,
-		payerErrorMessage,
+		addButton,
+		wordErrorMessage,
+		meaningErrorMessage,
 	};
 };
 
 describe("퀴즈 메인 페이지", () => {
 	describe("퀴즈 추가 컴포넌트", () => {
 		test("퀴즈 추가 컴포넌트 렌더링", () => {
-			const { wordinput, meaningInput } = renderComponent();
+			const { wordinput, meaningInput, addButton } = renderComponent();
 
 			expect(wordinput).toBeInTheDocument();
 			expect(meaningInput).toBeInTheDocument();
+			expect(addButton).toBeInTheDocument();
+		});
+
+		test('퀴즈 추가에 필수적인 값을 입력하지 않고 "추가" 버튼 클릭시, 에러 메시지를 노출한다', async () => {
+			const { addButton, wordErrorMessage, meaningErrorMessage } =
+				renderComponent();
+
+			expect(addButton).toBeInTheDocument();
+			await userEvent.click(addButton);
+
+			expect(wordErrorMessage).toHaveAttribute("data-valid", "false");
+			expect(meaningErrorMessage).toHaveAttribute("data-valid", "false");
+		});
+
+		test('퀴즈 추가에 필수적인 값들을 입력한 후 "추가" 버튼 클릭시, 저장에 성공', async () => {
+			const {
+				wordinput,
+				meaningInput,
+				addButton,
+				wordErrorMessage,
+				meaningErrorMessage,
+			} = renderComponent();
+
+			await userEvent.type(wordinput, "happy");
+			await userEvent.selectOptions(meaningInput, "행복한");
+			await userEvent.click(addButton);
+
+			expect(wordErrorMessage).toHaveAttribute("data-valid", "true");
+			expect(meaningErrorMessage).toHaveAttribute("data-valid", "true");
+		});
+	});
+
+	describe("새로운 퀴즈가 입력 되었을 때,", () => {
+		const addNewQuiz = async () => {
+			const { wordinput, meaningInput, addButton } = renderComponent();
+			await userEvent.type(wordinput, "pencil");
+			await userEvent.type(meaningInput, "연필");
+			await userEvent.click(addButton);
+		};
+		test("단어와 뜻 데이터가 퀴즈 리스트에 추가 된다", async () => {
+			await addNewQuiz();
+
+			const quizListComponent = screen.getByTestId("quizList");
+			const dateValue = within(quizListComponent).getByText("pencil");
+			expect(dateValue).toBeInTheDocument();
+
+			const descValue = within(quizListComponent).getByText("연필");
+			expect(descValue).toBeInTheDocument();
 		});
 	});
 });
