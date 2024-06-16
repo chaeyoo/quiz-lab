dca257a55460741fde815eee823ff67e955998b1
chaeyoo <chaeh0124@gmail.com>
2024-06-16 22:27:23 +0900

    [FEAT] 퀴즈 목록 react-query

--- src/components/quiz-set-list/QuizSetList.tsx
+++ src/components/quiz-set-list/QuizSetList.tsx
@@ -1,8 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import { IQuizSet } from "../../types/quiz";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useInfiniteQuery } from "react-query";
+import { getQuizSetList } from "../../remote/quizes";
+import { useCallback } from "react";
+import { flatten } from "lodash";
 
 export default function QuizSetList({ quizSets }: { quizSets: IQuizSet[] }) {
 	const navigate = useNavigate();
+	const {
+		data,
+		hasNextPage = false,
+		fetchNextPage,
+		isFetching,
+	} = useInfiniteQuery(
+		["quizes"],
+		({ pageParam }) => {
+			return getQuizSetList(pageParam);
+		},
+		{
+			getNextPageParam: (snapShot) => {
+				return snapShot.lastVisible;
+			},
+		}
+	);
+	const loadMore = useCallback(() => {
+		if (!hasNextPage || isFetching) {
+			return;
+		}
+		fetchNextPage();
+	}, [fetchNextPage, hasNextPage, isFetching]);
+	console.log(data);
+	const quizSetList: any = flatten(data?.pages.map(({ items }) => items));
 	return (
 		<div className="mx-16 my-14">
 			<h1 className="text-4xl font-extrabold mb-7">퀴즈랩</h1>
@@ -13,7 +42,7 @@ export default function QuizSetList({ quizSets }: { quizSets: IQuizSet[] }) {
 				/>
 				<div data-testid="quiz-sets">
 					{(!quizSets || quizSets.length === 0) && <>비어있음</>}
-					{quizSets &&
+					{/* {quizSets &&
 						quizSets.length > 0 &&
 						quizSets.map((set, idx) => (
 							<li
@@ -40,7 +69,41 @@ export default function QuizSetList({ quizSets }: { quizSets: IQuizSet[] }) {
 								</div>
 								<div data-testid="set-author">{set.author}</div>
 							</li>
+						))} */}
+					<InfiniteScroll
+						dataLength={quizSets.length}
+						hasMore={hasNextPage}
+						loader={<>Loading...</>}
+						next={loadMore}
+						scrollThreshold={"600px"}
+					>
+						{quizSetList.map((v: IQuizSet, idx: number) => (
+							<li
+								className="list-none mb-3 rounded-lg p-5 border-2 border-gray-400 border-opacity-50"
+								key={v.id}
+								data-testid="set-list"
+								onClick={() => {
+									navigate(`/quiz/${v.id}`);
+								}}
+							>
+								<div
+									className="text-lg font-semibold mb-2"
+									data-testid="set-title"
+								>
+									{v.name}
+								</div>
+								<div className="mb-10">
+									<span
+										className="text-xs bg-slate-500 px-2 py-1 rounded-3xl font-semibold text-white"
+										data-testid="set-length"
+									>
+										{v.length} 단어
+									</span>
+								</div>
+								<div data-testid="set-author">{v.author}</div>
+							</li>
 						))}
+					</InfiniteScroll>
 				</div>
 			</div>
 		</div>
