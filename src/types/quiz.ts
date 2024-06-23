5f2a5c23fede5c22e080c611f3343ad59addc6ad
chaeyoo <chaeh0124@gmail.com>
2024-06-23 22:46:48 +0900

    Revert "[FEAT] 퀴즈 상세 작업 중"
    This reverts commit 67400706ca734395bf452fffd741c0fd4ab98ac2.

--- src/types/quiz.ts
+++ src/types/quiz.ts
@@ -1,13 +1,7 @@
-export interface Quiz {
-	word: string;
-	mean: string;
-	seq: number;
-}
 export interface IQuizSet {
 	id: number;
 	name: string;
 	length: number;
 	author: string;
 	createdAt: string;
-	quizes?: Quiz[];
 }
